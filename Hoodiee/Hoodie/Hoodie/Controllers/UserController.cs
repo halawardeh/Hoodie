da551cbc61a919311d0f7389d86f40fad4d8c308
halawardeh <halawardeh12102001@gmail.com>
2025-04-03 17:53:02 +0300

    Change the theme

---
+++ Hoodiee/Hoodie/Hoodie/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hoodie.Controllers
+{
+    public class UserController : Controller
+    {
+
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        public IActionResult AllOrders()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Hoodiee/Hoodie/Hoodie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hoodie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
